57ee3ad251503d6e8c16894b8ec4146411b8bfb2
sblixen-finecke <sblixen-finecke@LAP-A121177.fsa.gov.uk>
2014-05-05 17:03:04 +0100

    Fixed deleteFromGit to work on windows

--- ant-salesforce-ext/src/java/dk/stfkbf/DeleteFromGit.java
+++ ant-salesforce-ext/src/java/dk/stfkbf/DeleteFromGit.java
@@ -56,14 +56,18 @@ public class DeleteFromGit {
 
 	public void traverse(Git git, File file) throws Exception{		
 		File targetDir = file;
-
+		
+		File sourcePath = new File(this.getSourcePath());
+		File gitRootPath = new File(this.getGitRoot());
+		File targetPath = new File(this.getTargetPath());
+		
 		if (file.exists()){
 			for (String currentFile : targetDir.list()){
 				File targetFile = new File(targetDir, currentFile);
 
-				File sourceFile = new File(targetFile.getAbsolutePath().replace(this.getTargetPath(), this.getSourcePath()));
+				File sourceFile = new File(targetFile.getAbsolutePath().replace(targetPath.getAbsolutePath(), sourcePath.getAbsolutePath()));
 
-				String fileName = targetFile.getAbsolutePath().replace(this.gitRoot + "/", "");
+				String fileName = targetFile.getAbsolutePath().replace(gitRootPath.getAbsolutePath() + "/", "");
 
 				if (targetFile.isFile() &&  !sourceFile.exists()) {
 					Set<String> list = git.status().addPath(fileName).call().getIgnoredNotInIndex();
